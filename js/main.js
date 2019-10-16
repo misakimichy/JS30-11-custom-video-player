c97430e1db7d4ec906afe4a65e0343fb2367d75a
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-15 18:48:18 -0700

    Add function to handle progress bar.

--- js/main.js
+++ js/main.js
@@ -24,10 +24,16 @@
         video[e.currentTarget.name] = e.currentTarget.value;
     };
 
+    const handleProgress = () => {
+        const percent = (video.currentTime / video.duration) * 100;
+        progressBar.style.flexBasis = `${percent}%`;
+    };
+
     // Event listeners
     video.addEventListener('click', togglePlay);
     video.addEventListener('play', updateButton);
     video.addEventListener('pause', updateButton);
+    video.addEventListener('timeupdate', handleProgress);
 
     toggle.addEventListener('click', togglePlay);
     skipButtons.forEach(skipButton => skipButton.addEventListener('click', skip));
